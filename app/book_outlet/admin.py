f2797bf95697d165b403e850dab57024131563e9
felrobotics <lposadaa@eafit.edu.co>
2024-01-24 23:56:02 -0500

    added address, author and country models and relationships

--- app/book_outlet/admin.py
+++ app/book_outlet/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Address, Author, Book, Country
+
 # Register your models here.
+# admin.site.register(Book) # simple registration, but we would like to config more
+
+
+# This allows for a better customization and overwriting
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ("slug",)
+    prepopulated_fields = {"slug2": ("title",)}
+    list_filter = ("rating",)
+    list_display = ("title", "author")
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address)
+admin.site.register(Country)
--- app/book_outlet/models.py
+++ app/book_outlet/models.py
@@ -1,9 +1,49 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
+
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.name}({self.code})"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(
+        to=Address, on_delete=models.CASCADE, null=True
+    )  # one to one
+
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+    def get_absolute_url(self):
+        return reverse("Author_detail", kwargs={"pk": self.pk})
 
 
 class Book(models.Model):
@@ -11,11 +51,18 @@ class Book(models.Model):
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
-    author = models.CharField(max_length=100, null=True)
+    # author = models.CharField(max_length=100, null=True) // original string
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name="books"
+    )
     is_bestselling = models.BooleanField(default=False)
-    # slug = models.SlugField(default="", null=False)  # orig code, but not so perfor
-    # makes db_index in slug get_absolute_url search more efficient
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    #  Lets show other option, this one will prepoluate the slug in the admin/
+    slug2 = models.SlugField(default="", blank=True, null=False, db_index=True)
+    # many to many creates behinds the scenes a 3rd mapping table
+    published_countries = models.ManyToManyField(
+        to=Country, null=False, related_name="books"
+    )
 
     def __str__(self) -> str:
         return f"{self.title} ({self.rating})"
