86440b5f1175965679e931b7948d581fdbb78340
felrobotics <lposadaa@eafit.edu.co>
2024-01-30 23:58:53 -0500

    + review app templates urls and views

--- app/reviews/views.py
+++ app/reviews/views.py
@@ -1,7 +1,23 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def review(request):
-    return render(request, "reviews/review.html")
+    print("DEBUG")
+    print(request.method)
+    if request.method == "POST":
+        entered_username = request.POST["username"]
+        print(entered_username)
+        # validate
+        if (not entered_username) or (len(entered_username) <= 10):
+            return render(request, "reviews/review.html", {"has_error": True})
+
+        return HttpResponseRedirect(redirect_to="thank-you")
+
+    return render(request, "reviews/review.html", {"has_error": False})
+
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
