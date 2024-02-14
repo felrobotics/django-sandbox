6d64382cb2668ef9e4797f84962ebbdd7e54b9d0
felrobotics <lposadaa@eafit.edu.co>
2024-02-14 09:27:28 -0500

    add profile app

--- app/core/settings/base.py
+++ app/core/settings/base.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 import os
 from pathlib import Path
 
@@ -51,6 +52,7 @@ INTERNAL_APPS = [
     "posts",
     "book_outlet",
     "reviews",
+    "profiles",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + EXTERNAL_APPS + INTERNAL_APPS
--- app/core/urls.py
+++ app/core/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import RedirectView
@@ -38,4 +39,5 @@ urlpatterns = [
     path("posts/", include("posts.urls")),
     path("book-outlet/", include("book_outlet.urls")),
     path("feedback/", include(arg="reviews.urls")),
+    path("profiles/", include(arg="profiles.urls")),
 ]
